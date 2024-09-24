d2f0968c3899664fedd6784644c2200d303c0d36
Andrey Chirkin <chirkin.andrey377@gmail.com>
2024-09-24 22:12:49 +0300

    Modified Tic Tac Toe 2.py

--- Modul 7) Repetition of themes/Tic Tac Toe 2.py
+++ Modul 7) Repetition of themes/Tic Tac Toe 2.py
@@ -1,6 +1,6 @@
-def print_board(board) -> None:
+def print_board(board: list[list[str]]) -> None:
     """
-    Функция, которая будет отображать текущее состояние игрового поля в консоли.
+    Функция отображает текущее состояние игрового поля в консоли.
     :param board:
     :return: None
     """
@@ -13,21 +13,19 @@ def print_board(board) -> None:
         print()
 
 
-def check_completion_game(board, number_player: int, mark: str) -> bool:
+def check_completion_game(board: list[list[str]], number_player: int, mark: str) -> bool:
     """
-    Проверка на завершенность игры. Функция вернет True в случае победы и в случае ничьи.
+    Функция проверяет завершенность игры.
     :param board:
     :param number_player:
     :param mark:
-    :return: bool
+    :return: Bool
     """
     if any([  # Условия победы
-        [mark]*3 in board,  # Проверка равенства значений в строке
-        set([board[index_row][0] for index_row in range(3)]) == set(mark),  # Проверка равенства в столбце с индексом 0
-        set([board[index_row][1] for index_row in range(3)]) == set(mark),  # Проверка равенства в столбце с индексом 1
-        set([board[index_row][2] for index_row in range(3)]) == set(mark),  # Проверка равенства в столбце с индексом 2
-        board[0][0] == board[1][1] == board[2][2] == mark,  # Проверка равенства значений по диагонале
-        board[0][2] == board[1][1] == board[2][0] == mark  # Проверка равенства значений по диагонале
+        [mark] * 3 in board,  # Проверка равенства значений в строке
+        (mark,) * 3 in list(zip(*board)),  # Проверка равенства значений в столбце
+        [mark] * 3 == [row[i] for i, row in enumerate(board)],  # Проверка равенства значений по диагонали
+        [mark] * 3 == [row[i] for i, row in enumerate(board[::-1])]  # Проверка равенства значений по противоположной диагонали
     ]):
         print(f"Игрок {number_player} победил!")
         return True
@@ -39,8 +37,15 @@ def check_completion_game(board, number_player: int, mark: str) -> bool:
         return False
 
 
-def input_data_player(board, number_player, mark):
-    while True:  # Игрок вводит данные пока не пройдут проверку
+def input_data_player(board: list[list[str]], number_player: int, mark: str) -> bool:
+    """
+    Функция обеспечивает ввод данных и проверку корректности ввода.
+    :param board:
+    :param number_player:
+    :param mark:
+    :return:
+    """
+    while True:
         print(f"\nИгрок {number_player} ваш ход")
         row_index = input("Введите индекс строки: ")
         if row_index in ('0', '1', '2'):
@@ -50,11 +55,8 @@ def input_data_player(board, number_player, mark):
                 col_index = int(col_index)
                 if board[row_index][col_index] not in ('X', '0'):
                     board[row_index][col_index] = mark
-                    if check_completion_game(board, number_player, mark):  # Проверка на завершенность игры
-                        print_board(board)
-                        return True
                     print_board(board)
-                    return False
+                    return check_completion_game(board, number_player, mark)  # Проверяет на завершенность игры
                 else:
                     print("Ячейка занята!")
                     print_board(board)
@@ -69,9 +71,13 @@ def input_data_player(board, number_player, mark):
             continue
 
 
-def input_data():
+def main() -> None:
+    """
+    Функция обеспечивает окончание выполнения программы в случае победы или ничьи.
+    :return: None
+    """
     print("-----Добро пожаловать в игру крестики нолики-----")
-    board: list[list[str]] = [
+    board = [
         ['-', '-', '-'],
         ['-', '-', '-'],
         ['-', '-', '-']
@@ -82,4 +88,4 @@ def input_data():
             break
 
 
-input_data()
+main()
